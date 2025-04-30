0326955a48c2cb7fbd92bd1cc76015c41b1c422a
Slawek <slawomirswitala81@gmail.com>
2025-04-30 19:46:12 +0200

    deleted build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo ">>> Installing dependencies"
-pip install -r requirements.txt
-
-echo ">>> Running collectstatic"
-python manage.py collectstatic --noinput
-
-echo ">>> Running migrations"
-python manage.py migrate
