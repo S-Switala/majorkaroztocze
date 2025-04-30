83414da8d8c933801568fe68bbe003262c103832
Slawek <slawomirswitala81@gmail.com>
2025-04-30 19:55:52 +0200

    empty commit

--- backend/settings.py
+++ backend/settings.py
@@ -100,10 +100,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',  # fallback lokalny
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # Password validation
